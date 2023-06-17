242bfe5f1f2ad74e749d8a722eb864cdda2f753c
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-06-16 20:32:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ *        followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 26; n++)
+	{
+		putchar('a' + n);
+		putchar('\n');
+	}
+	return (0);
+}
+
