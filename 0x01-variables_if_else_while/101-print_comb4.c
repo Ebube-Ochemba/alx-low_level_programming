6c5c1f38f0fddc8dbe29fa173c8c89bd85dafd5f
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-06-17 19:09:03 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different
+ *        combinations of three digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x, y, z;
+	int count = 0;
+
+	for (x = '0'; x <= '7'; x++)
+	{
+		for (y = x + 1; y <= '8'; y++)
+		{
+			for (z = y + 1; z <= '9'; z++)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+				if ((x == 7) && (y == 8) && (z == 9))
+				{
+					putchar('\n');
+				}
+				else if (count < 120)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				count++;
+			}
+		}
+	}
+	return (0);
+}
