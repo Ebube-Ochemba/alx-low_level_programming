c67e164b9ef530cb936f10320de7894cd52275b8
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-06-26 07:57:55 -0700

    9-fizz_buzz.c correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Fizz, Buzz & FizzBuzz ;).
+ * main - print 1 to 100,
+ *        multiples of 3 with Fizz,
+ *        multiples of 5 with Buzz,
+ *        and and multiple of both 3 and 5 with FizzBuzz.
  *
  * Return: Always 0 (Success).
  */
@@ -14,19 +17,23 @@ int main(void)
 	{
 		if ((az % 3 == 0) && (az % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if  (az % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (az % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", az);
+			printf("%d", az);
+		}
+		if (az < 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
