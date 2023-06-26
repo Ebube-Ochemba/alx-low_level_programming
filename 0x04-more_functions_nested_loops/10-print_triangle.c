582aa6d7cf503ff44808f2e2dadd6dc5ed17fe8b
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-06-26 09:11:43 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed
+ *                  by a new line.
+ * @size: The size of the triangle.
+ *
+ * Return: Always 0 (SUccess)
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - 1; y >= 0; y--)
+			{
+				if (y > x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+				_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
