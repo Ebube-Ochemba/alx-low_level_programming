88069279623172a50e3116a0178d0541b944b9dd
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-07-01 13:26:34 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed
+ *             by a new line.
+ * @s: The string to be printed.
+ *
+ * Return: Void.
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int reverse;
+
+	/** function to find the length of the string */
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	/** To go back to charecter before null charecter */
+	s--;
+
+	/** Now, print reverse string */
+	for (reverse = length; reverse > 0; reverse--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
