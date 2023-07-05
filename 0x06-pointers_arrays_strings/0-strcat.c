20b73e5d5a72eb0d6e9b3cd7ae15f1351b51255c
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-07-05 16:52:30 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the src string to the dest string,
+ *           overwriting the terminating null byte (\0)
+ *           at the end of dest, and then adds a
+ *           terminating null byte.
+ *
+ * @src: The ssource string.
+ * @dest: The destination string.
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int didx = 0;
+	int sidx = 0;
+
+	/** A loop to put pointer at string end */
+	while (dest[didx] != '\0')
+	{
+		didx++;
+	}
+
+	while (src[sidx] != '\0')
+	{
+		dest[didx] = src[sidx];
+		didx++;
+		sidx++;
+	}
+	dest[didx] = '\0';
+
+	return (dest);
+}
