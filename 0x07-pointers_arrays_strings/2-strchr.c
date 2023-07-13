a46fd0997f0db4488098beedddb7a57f49156b6d
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-07-12 18:47:23 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be searched for.
+ *
+ * Return: A pointer to the first occurence of the
+ *         character if found, else NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
