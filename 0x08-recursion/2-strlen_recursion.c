9ac733ec189dcc1f07ef80dfe4b9cccf24997e20
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-07-21 16:49:30 -0700

    2-strlen_recursion.c comments

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,13 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == '\0') /* Base case */
 	{
 		return (0);
 	}
 	else
 	{
+		/* Non-tail recursive call */
 		return (1 + _strlen_recursion(s + 1));
 	}
 }
