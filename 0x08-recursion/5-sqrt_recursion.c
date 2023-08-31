2f99ba37e0cd830cf0cd05c55bfe0f1823e55398
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-08-31 12:01:22 +0000

    5-sqrt1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: An in to calculate its square root.
- *
- * Return: -1 if square root is unnatural,
- *         else returns square root.
- */
-
-int find_sqrt(int num, int root);
-
-int _sqrt_recursion(int n)
-{
-	if (n < 0) /* base case */
-	{
-		return (-1);
-	}
-
-	return (find_sqrt(n, 0));
-}
-
-/**
  * find_sqrt - starts from 0 to fint the squareroot of n.
  * @num: Integer to be passed.
  * @root: Test toot.
@@ -32,7 +12,7 @@ int find_sqrt(int num, int root)
 {
 	if ((root * root) > num) /* sqrt of a num can't be > root^2 */
 	{
-		return (-1);
+	return (-1);
 	}
 	else if ((root * root) == num)
 	{
@@ -40,6 +20,25 @@ int find_sqrt(int num, int root)
 	}
 	else
 	{
-		return (find_sqrt(num, root + 1)); /* direct recursion */
+	return (find_sqrt(num, root + 1)); /* direct recursion */
 	}
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: An in to calculate its square root.
+ *
+ * Return: -1 if square root is unnatural,
+ *         else returns square root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0) /* base case */
+	{
+		return (-1);
+	}
+
+	return (find_sqrt(n, 0));
 }
