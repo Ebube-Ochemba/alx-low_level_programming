f05911eae7b7c7bc113f96036b2a72cbe258aa8e
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-06 13:34:36 +0000

    2-str_concat.c2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,15 @@ char *str_concat(char *s1, char *s2)
 	int len, len2 = 0;
 	char *array;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	while (s1[len1] != '\0')
 	{
 		len1++;
