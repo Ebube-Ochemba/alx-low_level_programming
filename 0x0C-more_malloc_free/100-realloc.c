5bd0ed5c59d5000edb1015c565a5afa5a7a14849
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-08 01:12:48 +0000

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,35 +16,35 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *block;
 	unsigned int idx, len = 0;
 
-	if (ptr == NULL)
+	if (ptr == NULL) /* check if ptr is NULL */
 	{
 		block = malloc(new_size);
 		return (block);
 	}
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL) /* check new_size */
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
+	if (new_size == old_size) /* compare sizes */
 	{
 		return (ptr);
 	}
 
-	len = (old_size < new_size) ? old_size : new_size;
+	len = (old_size < new_size) ? old_size : new_size; /* choose size */
 
 	block = malloc(new_size);
-	if (block == NULL)
+	if (block == NULL) /* check malloc */
 	{
 		return (NULL);
 	}
 
-	for (idx = 0; idx < len; idx++)
+	for (idx = 0; idx < len; idx++) /* copy content*/
 	{
 		((char *)block)[idx] = ((char *)ptr)[idx];
 	}
 
-	free(ptr);
+	free(ptr); /* free old memory */
 
 	return (block);
 }
