689da06fea35ac6abe822ae193249611d602de2c
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-11 21:25:44 +0000

    struct_dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dogs.
+ * @name: 1st member.
+ * @age: 2nd member.
+ * @owner: 3rd member.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
