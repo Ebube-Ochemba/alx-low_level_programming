23725679548659bbabaa7b28dacf3f374f14637c
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-11 23:07:58 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL) /* validate initialization of d */
 	{
-		d = malloc(sizeof(struct dog));
+		return;
 	}
 
 	d->name = name;
