42eab12f5248ee592658fb158a53bfeae4a0d302
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-11 22:00:12 +0000

    validate_initialization

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,6 +13,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL) /* validate initialization of d */
+	{
+		return;
+	}
+
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
