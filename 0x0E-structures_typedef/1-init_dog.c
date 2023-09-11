19f931db76aea0575385b2674f28ee60286d92cc
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-11 22:47:44 +0000

    tried_malloc

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct dog.
@@ -13,7 +13,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL) /* validate initialization of d */
 	{
-		return;
+		d = malloc(sizeof(struct dog));
 	}
 
 	d->name = name;
