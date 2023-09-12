67fe8d3a92c01970469f842a7a0db019e983df8d
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-12 12:54:39 +0000

    header_update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,24 +2,22 @@
 #define DOG_H
 
 /**
+ * dog_t - typedef for struct dog.
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - structure for dogs.
  * @name: 1st member.
  * @age: 2nd member.
  * @owner: 3rd member.
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - typedef for struct dog.
- */
-
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
