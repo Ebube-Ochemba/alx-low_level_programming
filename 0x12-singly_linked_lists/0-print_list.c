40ee8a44ad16a0afb9afbb0f15a0857ea4608b5b
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-22 01:50:27 +0000

    betty fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h!= NULL) /* traverse struct */
+	while (h != NULL) /* traverse struct */
 	{
 		if (h->str == NULL) /* check pointer */
 		{
