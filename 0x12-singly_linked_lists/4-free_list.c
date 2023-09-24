72da9cfb39a04bc87f529118e97ac2bdeb1bf089
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-24 12:55:09 +0000

    update_and_comments

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,10 +7,13 @@
 
 void free_list(list_t *head)
 {
-	while (head != NULL)
+	list_t *tmp;
+
+	while (head != NULL) /* traverse nodes */
 	{
-		free(head->str);
-		free(head);
-		head = head->next;
+		tmp = head; /* temporarily store current head */
+		head = head->next; /* move head to next node */
+		free(tmp->str); /* free tmp member */
+		free(tmp); /* free tmp */
 	}
 }
