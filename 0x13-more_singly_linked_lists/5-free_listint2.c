270aa0e41e6a58a580406ced701f44bad82d0dca
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-25 12:57:48 +0000

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free linked list
+ * @head: a double pointer to a linked list
+ *
+ * Return: Void.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL) /* traverse nodes */
+	{
+		tmp = *head; /* temporarily store current head */
+		*head = (*head)->next; /* move head to next node */
+		free(tmp); /* free tmp */
+	}
+}
