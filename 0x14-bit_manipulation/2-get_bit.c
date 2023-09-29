9090a33d56d5cbc8a1f24c9e7141d1fda33532bd
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-29 00:06:59 +0000

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,13 +2,27 @@
 
 /**
  * get_bit - eturns the value of a bit at a given index.
- * @n: 
- * @index:
+ * @n: The number to work on.
+ * @index: The index.
  *
- * Return:
+ * Return: Value of bit at index, else -1 for error.
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	x;
+	unsigned int shift, bit_val, num_bits;
+
+	/* calculate #bits in an unsigned long int & convert to bits */
+	num_bits = (sizeof(unsigned long int) * 8);
+
+	if (index > num_bits) /* check if index is valid */
+	{
+		return (-1);
+	}
+
+	shift = n >> index; /* shift = n / 2^index */
+
+	bit_val = shift & 1; /* bitwise AND result */
+
+	return (bit_val);
 }
