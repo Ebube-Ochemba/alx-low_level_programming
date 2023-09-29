1888b7307b0a01fc541e4b265148eee69054b031
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-29 17:06:41 +0000

    whitespace

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -12,7 +12,7 @@ int get_endianness(void)
 	/* store hex value of 1 in a 4 byte int (0x00000001) */
 	/* Use a pointer to check if first byte stored is 00 or 01 */
 	/* big endian would store 00, little would store 01 */
-	
+
 	int uno = 0x00000001;
 	char *byte = (char *)&uno;
 
