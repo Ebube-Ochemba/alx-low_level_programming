c68073dbb2b97872904f8981c5bce234e49221b2
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-09-29 17:03:58 +0000

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness.
+ * get_endianness - Checks the Endianness.
+ *                  (i.e. How your computer stores numbers in memory).
  *
- * Return: first value stored, 0 if big endian, 1 if little.
+ * Return: first value stored (0 if big endian, 1 if little endian).
  */
 
 int get_endianness(void)
 {
-	int x;
+	/* store hex value of 1 in a 4 byte int (0x00000001) */
+	/* Use a pointer to check if first byte stored is 00 or 01 */
+	/* big endian would store 00, little would store 01 */
+	
+	int uno = 0x00000001;
+	char *byte = (char *)&uno;
+
+	return (byte[0]);
 }
