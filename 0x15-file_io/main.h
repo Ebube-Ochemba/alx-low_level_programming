c2b18ce29e8c2d4279d3094ca4054cfb5a12f791
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-10-03 02:09:14 +0000

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <unistd.h>
+/* open */
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/* close */
+#include <unistd.h>
+
+/* dprintf */
+#include <stdio.h>
+
+/* malloc, free */
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
