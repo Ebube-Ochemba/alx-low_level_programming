4e83b0105f4bf56a8a9c7a721210f6da199c0d56
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-09 22:29:01 +0000

    task4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * free_dlistint_t - Frees a dlistint_t list.
- * @head: doubly linked list.
+ * free_dlistint - Frees a dlistint_t list.
+ * @head:A doubly linked list.
  *
  * Return: Void.
  */
 
-void free_dlistint_t(dlistint_t *head)
+void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 
