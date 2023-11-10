869e0269aa574c3e90a3bb2300ad82601cf2901c
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-10 22:28:16 +0000

    task7cor

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,12 +26,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0) /* assign new_node as head */
 	{
 		new_node->next = *h;
+		(*h)->prev = new_node;
 		*h = new_node; /* update head of list to new_node */
 		return (new_node);
 	}
 
 	current = *h; /* temporarily store current node */
-
 	while (current != NULL && count < idx - 1) /* find valid idx */
 	{
 		current = current->next; /* move to next node */
@@ -45,6 +45,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	new_node->next = current->next; /* update *next of new node */
+	new_node->next->prev = new_node;
 	current->next = new_node; /* update *next of current node */
 	new_node->prev = current; /* update *prev of new node */
 	return (new_node);
