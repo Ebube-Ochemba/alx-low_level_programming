bf4d108953990f96a9d14e9de61c86ddcf1c7737
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-03 17:28:42 +0000

    commented djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,12 +9,25 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int c;
+	int c; /* Holds ASCII value of current charecter */
 
-	hash = 5381;
+	hash = 5381; /* Magic number for good hash values */
+
+	/**
+	 * 1. Get the ASCII value of the current character, hash it
+	 *    and then increment the pointer to the next character
+	 *    of the string.
+	 * 2. The loop until it encounters a character with an ASCII
+	 *    value of 0 (i.e the NULL byte or end of string).
+	 */
 	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		/**
+		 * Updating hash value.
+		 * 1. (hash << 5) + hash == hash * 33, but faster.
+		 * 2. Add the result c.
+		 */
 	}
-	return (hash);
+	return (hash); /* return hashed string */
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table.
+ * @key: The key.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if successsful, else 0.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	x
