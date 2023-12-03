4d6a846ab730485a643890d98a8b9bc3c440bdbc
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-03 00:44:48 +0000

    task2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * unsigned long int key_index - Gives you the index of a key.
+ * key_index - Gives you the index of a key.
  * @key: The key.
  * @size: The size of the array of the hash table.
  *
