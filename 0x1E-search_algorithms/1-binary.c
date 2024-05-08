918034c17bfd695903843f8b4c7c1de024d397b6
Ebube-Ochemba <ebube17@gmail.com>
2024-05-08 16:26:26 +0100

    completed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,5 +12,38 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	1
+	size_t idx, mid, left = 0, right = size - 1;
+
+	if (!array || !size || !value)
+		return (-1);
+
+	while (left <= right)
+	{
+		/* Print search range */
+		printf("Searching in array: ");
+		for (idx = left; idx <= right; idx++)
+		{
+			printf("%d", array[idx]);
+			if (idx < right)
+				printf(", ");
+		}
+		printf("\n");
+
+		/* Set middle */
+		mid = left + ((right - left) / 2);
+
+		/* Divide and conquer algo */
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+
+	return (-1);
 }
