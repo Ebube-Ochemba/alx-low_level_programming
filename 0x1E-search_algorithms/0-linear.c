a5745ea4847ce0463df1fa5a615ac64882b3d930
Ebube-Ochemba <ebube17@gmail.com>
2024-05-08 13:55:45 +0100

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,13 +15,13 @@ int linear_search(int *array, size_t size, int value)
 		size_t idx;
 
 		if (!array || !size || !value)
-				return (-1);
+			return (-1);
 
 		for (idx = 0; idx < size; idx++)
 		{
-				printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
+			printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
 				if (array[idx] == value)
-						return (idx);
+					return (idx);
 		}
 		return (-1);
 }
